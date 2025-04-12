733f50de9af647f4f265a33d4c2215323a4f794d
Emil Skog <emil@skog.io>
2025-04-12 12:45:57 +0200

    removed //nosec workaround for cleanPath

--- internal/helpers/filecopy.go
+++ internal/helpers/filecopy.go
@@ -14,11 +14,11 @@ func Filecopy(src, dst string) error {
 	if err != nil {
 		return fmt.Errorf("error sanitizing filepaths: %w", err)
 	}
-	content, err := os.ReadFile(src) // #nosec G304 Due to false alarm, the paths are sanitized in the call to verifypaths
+	content, err := os.ReadFile(filepath.Clean(src))
 	if err != nil {
 		return fmt.Errorf("unable to read template file %s: %w", src, err)
 	}
-	file, err := os.Create(dst) // #nosec G304 Due to false alarm, the paths are sanitized in the call to verifypaths
+	file, err := os.Create(filepath.Clean(dst))
 	if err != nil {
 		return fmt.Errorf("unable to create empty destination file:%s: %w", dst, err)
 	}
@@ -33,8 +33,6 @@ func Filecopy(src, dst string) error {
 }
 
 func verifypaths(src, dst string) (string, string, error) {
-	src = filepath.Clean(src)
-	dst = filepath.Clean(dst)
 	if !strings.HasPrefix(src, basePath) {
 		return "", "", fmt.Errorf("invalid path to template file: %s", src)
 	}
